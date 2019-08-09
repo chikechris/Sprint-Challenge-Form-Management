f48ff206510bf0e9ef53b8727cb8df9df0ea427e
Chike Chris <cchike2017@gmail.com>
2019-08-09 12:19:54 -0400

    modified Userform,js component to class component

--- form-management/src/components/UserForm.js
+++ form-management/src/components/UserForm.js
@@ -3,21 +3,30 @@ import { withFormik, Form, Field } from "formik";
 import axios from "axios";
 import * as Yup from "yup";
 
-function UserForm({ touched, errors }) {
-  return (
-    <div className="form">
-      <h3>User Form</h3>
-      <Form>
-        <label htmlFor="username">Username: </label>
-        <Field type="text" name="username" placeholder="Enter Username" />
-        {touched.username && errors.username && <p>errors.username</p>}
-        <label htmlFor="password">Password: </label>
-        <Field type="password" name="password" placeholder="Enter Password" />
-        {touched.password && errors.password && <p>errors.password</p>}
-        <button>Submit</button>
-      </Form>
-    </div>
-  );
+class UserForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="form">
+        <h3>User Form</h3>
+        <Form>
+          <label htmlFor="username">Username: </label>
+          <Field type="text" name="username" placeholder="Enter Username" />
+          {this.props.touched.username && this.props.errors.username && (
+            <p>{this.props.errors.username}</p>
+          )}
+          <label htmlFor="password">Password: </label>
+          <Field type="password" name="password" placeholder="Enter Password" />
+          {this.props.touched.password && this.props.errors.password && (
+            <p>{this.props.errors.password}</p>
+          )}
+          <button>Submit</button>
+        </Form>
+      </div>
+    );
+  }
 }
 
 export default withFormik({
